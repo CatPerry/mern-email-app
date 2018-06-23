7179be1496b4d0eb01f5f48a2b2c0fb113a9c555
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-06-23 16:33:14 -0400

    passportjs and google oauth started

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 //the above is a common JS module type, it's s system of requiring other files, rather than using import which is for es2015 apps—this is for ths Node side of the app. For React we can use import
 //you can for th ebleow have multiples instances of APP/ 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
+//this needs a CLIETN ID and  CLIENT SECRET
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      }, accessToken => {
+        console.log(accessToken);
+        }
+      )
+);
+
 //this is a route handler
-app.get('/', (req, res) => {
-  res.send({hi: "there"});
-});
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+  scope: ['profile', 'email']
+  })
+);
 //this listens for herokus dynamic port in a prodcution env.
-const PORT = process.ENV.port || 5000
+const PORT = process.env.port || 5000
 app.listen(PORT);
