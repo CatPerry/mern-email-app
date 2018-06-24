8f017f149d4ec2a99237e9f269185f13227d9e8a
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-06-23 20:34:03 -0400

    adding mongoose and mongo

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 //the above is a common JS module type, it's s system of requiring other files, rather than using import which is for es2015 apps—this is for ths Node side of the app. For React we can use import
 //you can for th ebleow have multiples instances of APP/ 
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 //since we're not using passort here in a typical way with new instances we dont even need the instance variable; just require etc. 
-require('./services/passport'); 
+require('./services/passport');
+require('./models/user');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 require('./routes/authRoutes')(app);
 
 //this listens for herokus dynamic port in a prodcution env.
-const PORT = process.env.port || 5000
+const PORT = process.env.port || 5000;
 app.listen(PORT);
