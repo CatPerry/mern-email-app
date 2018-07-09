7b92c2607f3d772ac10f7530eba9757787f425dc
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-07-09 11:25:38 -0400

    stripe key

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,9 +6,19 @@ import * as actions from '../actions';
 class Payments extends Component {
   render() {
 
-    return <StripeCheckout name="Emaily" description="Add $5 for 5 email credits" amount={500} token={token => this.props.handleToken(token)} stripeKey={process.env.STRIPE_PUBLISHABLE_KEY}>
-        <button className="btn">Add Credits</button>
-      </StripeCheckout>;
+    return (
+      <StripeCheckout 
+        name="Emaily"
+        description="Add $5 for 5 email credits"
+        amount={ 500 }
+        token={ token => this.props.handleToken(token) }
+        stripeKey={ process.env.REACT_APP_STRIPE_KEY }
+      >
+      <button className="btn">
+      Add Credits
+      </button>
+      </StripeCheckout>
+    );
   }
 }
 
